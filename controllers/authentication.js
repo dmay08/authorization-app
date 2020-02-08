ae97b696be062b49b7ae79ede2455e64ce1e197e
Danny Mayasich <dmayasich@macmini-ops05.avtr.net>
2020-02-07 17:35:17 -0800

    install 'jwt-simple' and add function 'tokenForUser(user)' to <authentication.js> as well as replace previous res.json with res.json({ token: tokenForUser(user) }) so that a user gets a JWT response from a POST to localhost:3090/signup, create <config.js> and create a random 'secret' string, then add 'config.js' to <gitignore.js> - next step is to decode the JWT

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,7 +1,16 @@
 const User = require('../models/user')
+const jwt = require('jwt-simple')
+const config = require('../config')
+
+function tokenForUser(user) {
+    const timestamp = new Date().getTime()
+    return jwt.encode({ sub: user.id, iat: timestamp }, config.secret)
+}
+
+// --------------------------- BELOW = first code written ---------- ABOVE = much later  -------------------------------
 
 exports.signup = function(req, res, next) {
-    // res.send({ success: 'true' }) // tested with this first (sent POST in postman to localhost:3090/signup >> didn't send anything >> got RESPONSE 'success: true' on page)
+    // res.send({ success: 'true' }) // tested with this first (sent POST in postman to localhost:3090/signup >> didn't send anything >> got RESPONSE 'success: true' on page) b/c of line 45 (res.json({success:true}))
     // console.log(req.body) // tested with this second (sent POST (in postman) to localhost:3090/signup >> sent JSON object: {"email": "example@gmail.com", "password": "abc" }) 
 
     const email = req.body.email
@@ -10,6 +19,11 @@ exports.signup = function(req, res, next) {
     // See if a user with the given email exists
     User.findOne({ email: email }, function(err, existingUser) {
         if (err) { return next(err) }
+
+        // If a user forgets to send either 'Email' or 'Password'
+        if (!email || !password) {
+            return res.status(422).send({ error: 'You must provide email and password' })
+        }
 
         // If a user with email DOES exist, return an error
         if (existingUser) {
@@ -27,8 +41,9 @@ exports.signup = function(req, res, next) {
             if (err) { return next(err) }
         })
 
-        // Respond to request indicating the user was created
-        res.json(user)
+        // Respond to request indicating the user was created >> send a JWT! (json web token)
+        // res.json({ success: true })   // after installing jwt-simple & writing 'tokenForUser()' >> I changed this code to below 
+        res.json({ token: tokenForUser(user) })
     })
 
 
--- models/user.js
+++ models/user.js
@@ -1,14 +1,38 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt-nodejs')
 
-// Define our model
+// 1) Define our model
 const userSchema = new Schema({
     email: { type: String, unique: true, lowercase: true },
     password: String
 })
 
-// Create the model class (to create new users)
+// 4) (way later after installing bcryptjs) >> On Save Hook, encrypt password (using bcrypt)
+    // Before saving a model, run this function
+userSchema.pre('save', function(next) {
+    // Get access to User model
+    const user = this 
+
+    // Generate a 'salt', then run callback
+    bcrypt.genSalt(10, function(err, salt) {
+        if (err) { return next(err) }
+
+        // Hash (encrypt) our password using the salt 
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if (err) { return next(err) }
+
+            // Overwrite plain text password with encrypted password
+            user.pasword = hash
+            
+            // Save the model
+            next()
+        })
+    })
+})
+
+// 2) Create the model class (to create new users)
 const ModelClass = mongoose.model('user', userSchema)
 
-// Export the model
+// 3) Export the model
 module.exports = ModelClass
